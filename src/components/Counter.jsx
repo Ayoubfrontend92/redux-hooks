ec6203e012144843f6c0f9dccc6a4de186190c9d
ayoub rezaei <ayoub.frontend1992@gmail.com>
2023-01-22 12:57:48 -0800

    redux-hooks

---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { increase } from '../redux/counter/counterAction'
+import { useSelector,useDispatch } from 'react-redux'
+
+const Counter = () => {
+
+  const counter  = useSelector(state =>state.counter)
+  const dispatch = useDispatch()
+  return (
+    <div>
+        <h1>Counter - {counter} </h1>
+        <button onClick={()=>dispatch(increase())}>Increase</button>
+    </div>
+  )
+}
+
+
+export default Counter;
